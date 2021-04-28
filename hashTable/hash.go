19eaaffd6fdcef4efe684d77e2affab27282aefd
ghaerdi <ghaerdi@ipisdosaj.edu.do>
2021-04-28 14:06:34 -0400

    testing dynamic properties with hastable using map in golang

---
+++ hashTable/hash.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+type element = map[int]string
+type hashTable struct {
+  items element 
+}
+
+func NewHashTable() (ht hashTable) {
+  ht.items = make(element)
+  return
+}
+
+func (ht *hashTable) Add(key, value string) {
+  i := hash(key)
+  ht.items[i] = value
+}
+
+func (ht *hashTable) Remove(key string) {
+  delete(ht.items, hash(key))
+}
+
+func (ht *hashTable) Get(key string) string {
+  index := hash(key)
+  return ht.items[index]
+}
+
+func hash(value string) (result int) {
+  for index, char := range value {
+    result = result + ascii(char) * index
+  }
+  return
+}
+
+func ascii(value rune) int {
+  return int(byte(value))
+}
+
+
+func main() {
+  example := NewHashTable()
+  example.Add("foo", "bar")
+  fmt.Println(example.Get("foo"))
+}
